b9016f2c9bf0cff0a0b3cd839c193b267c37b975
Jamal <cory.jy.lee@gmail.com>
2017-06-06 02:37:29 -0400

    added pause

--- cho.js
+++ cho.js
@@ -1,27 +1,30 @@
-function openTab(evt, movieName) {
-    var i, tabcontent, tablinks;
-	
-	var vid1 = document.getElementById("SE1E1");
-	var vid2 = document.getElementById("SE1E2");
-	var vid3 = document.getElementById("SE2E1");
+function openTab(evt, movieName) {  
+  //FIXME: THIS ENTIRE SHIT
+  const vid1 = document.getElementById("SE1E1");
+  const vid2 = document.getElementById("SE1E2");
+  const vid3 = document.getElementById("SE2E1");
 
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-	
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  const tablinks = document.getElementsByClassName("tablinks");
 
-	pauseVid();
-    document.getElementById(movieName).style.display = "block";
-    evt.currentTarget.className += " active";
+  tabcontent.forEach((tabcontent) => {
+    tabcontent.style.display = "none";
+  })
+
+  tablinks.forEach((tablinks) => {
+    tablinks.className = tablinks.className.replace(" active", "");
+  })
+
+  pauseVid();
+  document.getElementById(movieName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
- function pauseVid(){
-    $('iframe[src*="//www.youtube.com/embed/"]').each(function(i) {
-      this.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-    });
-  }
+function pauseVid(){
+  $('iframe[src*="//www.youtube.com/embed/"]').each(function(i) {
+    this.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+  });
+  let vidMeiFrame = $("#vid3").detach();
+  vidMeiFrame.appendTo("#SE2E1");
+  vidMeiFrame = null;
+}
